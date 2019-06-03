f5f97f0a98d6afe156c6a203c151e7ee52f75cd4
Chris Somerlot <CSomerlot@Brwncald.com>
2019-06-03 08:39:58 -0400

    cleanup

--- s3.bat
+++ s3.bat
@@ -1,2 +1,2 @@
 call %ALLUSERSPROFILE%/Anaconda3/Scripts/activate.bat
-aws s3 sync ./ s3://projects.somerlot.net --exclude "*" --include "*.html" --include "*.png" --include "*.jpg" --include "*.svg" --include "*.pdf" --include "*.xslx" --acl public-read --delete
+aws s3 sync ./ s3://projects.somerlot.net --exclude "*" --include "*.html" --include "*.png" --include "*.jpg" --include "*.svg" --include "*.pdf" --acl public-read --delete
