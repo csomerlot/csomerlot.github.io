03335fbaa9768cad202a2e9b415147d59a9f43d3
Chris Soerlot <csomerlot@gmail.com>
2019-09-16 09:15:03 -0400

    codeing progress

--- code/TartanDesigner.py
+++ code/TartanDesigner.py
@@ -1,55 +1,56 @@
 from turtle import *
-from time import sleep
-# from PIL import Image
 
+w = 2
+set = tuple(['green']*12 + ['red']*20) * 2
 
-maxDim = 200
-setup(width=maxDim+10, height=maxDim+10)
+maxDim =len(set)*w
+setup(width=maxDim, height=maxDim)
 screensize(maxDim, maxDim)
 setworldcoordinates(0,maxDim,maxDim,0)
 
-width(5)
-speed(10)
+width(w)
+speed(0)
+register_shape("bar",( (w/2,0),(-w/2,0),(-w/2,w),(w/2,w) ))
+shape("bar")
 
 def tossShuttle():
-    # print ("starting shuttle toss", heading(), pos())
     if heading() == 0.0:
         left(90)
-        forward(width())
+        forward(w)
         left(90)
         setx(maxDim)
     else:
         right(90)
-        forward(width())
+        forward(w)
         right(90)
         setx(0)
-    print ("leaving shuttle toss", heading(), pos())
-    # sleep(1)
 
 #warp
 home()
-color('red')
-for x in range(0,maxDim,width()*2):
-    goto([x, maxDim])
-    goto([x+width(), maxDim])
-    goto([x+width(), 0])
-    goto([x+width()*2, 0])
+for x, c in enumerate(set):
+    penup()
+    goto([x*w, -3*w])
+    pendown()
+    color(c)
+    goto([x*w, maxDim+3*w])
+penup()
 
 #weft
 home()
-color('green')
-while ycor() < maxDim:
-    for i in range(4):
-        forward(i*width())
-        while 0 <= xcor() <= maxDim:
-            forward(2*width())
-            penup()
-            forward(2*width())
-            pendown()
-        tossShuttle()
+start = 0
+for x, c in enumerate(set):
+    color(c)
+    treadle = start
+    while 0 <= xcor() <= maxDim:
+        if treadle < 2: stamp()
+        treadle +=1
+        if treadle > 3: treadle = 0
+        forward(w)
+    tossShuttle()
+    start +=1
+    if start > 3: start = 0
 
-
-getcanvas().postscript(file=__file__+".ps", colormode='color')
+# getcanvas().postscript(file=__file__+".ps", colormode='color')
 
 # done()
 exitonclick()
