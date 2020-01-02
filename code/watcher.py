b9200bdc30990d501eb6fac72863d23c75cc7e6b
Chris Soerlot <csomerlot@gmail.com>
2020-01-01 22:30:04 -0500

    coding

--- code/watcher.py
+++ code/watcher.py
@@ -1,11 +1,17 @@
-import socket, sys, os, csv
+import socket, sys, os, time
 import subprocess as sub
+
+with open(__file__.replace(".py", ".csv"), 'w', encoding='utf-8', newline='\n') as csvf:
+    csvf.write("datetime,path,size\n")
+lastmin = time.localtime()[4]
 
 def log(message):
     print(message)
 
-def dbInsert(dbconn, row):
-    dbconn.writerow(row)
+def dbInsert(dbconn, data):
+    with open(__file__.replace(".py", ".csv"), 'a', encoding='utf-8', newline='\n') as csvf:
+        for row in data:
+            csvf.write("{},{},{}\n".format(time.strftime("%m/%d/%Y %H:%M", time.localtime()), row, data[row]))
 
 def revLookup(ipaddr):
     ## not really needed since tcpdump already looks this up if it can
@@ -23,52 +29,62 @@ def lookupMAC(MAC):
     return MAC
 
 if __name__ == '__main__':
-    csvf = open(__file__.replace(".py", ".csv"), 'w', encoding='utf-8', newline='\n')
-    csvwriter = csv.DictWriter(csvf, ['time','type','length', 'fromMAC', 'fromADD','fromPort','fromDomain','toMAC','toADD', 'toPort', 'toDomain'])
-    csvwriter.writeheader()
+    db = {}
 
     ## run tcpdump in buffer mode, link headers only
-    # p = sub.Popen(('sudo', 'tcpdump', '-q', '-i', 'wlan1', '-le', 'greater 55 and tcp', '-c', '50'), stdout=sub.PIPE)
-    # for row in iter(p.stdout.readline, b''):
+    p = sub.Popen(('sudo', 'tcpdump', '-q', '-i', 'wlan1', '-le', 'greater 55 and tcp', '-c', '500'), stdout=sub.PIPE)
+    for row in iter(p.stdout.readline, b''):
 
     ## or use a test file for debugging
-    f = open('test.log', 'rb')
-    for row in f:
+    # with open('test.log', 'rb') as f: testlog = f.readlines()
+    # for row in testlog:
 
         try:
             mainParts =  row.decode('utf-8').strip().split(',')
-            timeStamp = mainParts[0].split(' ')[0]
-            fromMAC, toMAC = mainParts[0].replace(timeStamp, "").replace(" (oui Unknown)", "").split('>')
-            typ = mainParts[1]
-            length = mainParts[2].split(':')[0]
-            fromADD, toADD = mainParts[2].split(':')[1].split('>')
-            fromPort = fromADD.strip().split('.')[-1]
-            toPort = toADD.strip().split('.')[-1]
-            fromADD = fromADD.replace('.'+fromPort+' ','').strip()
-            toADD = toADD.replace('.'+toPort,'').strip()
-            if fromADD.replace('.','').isnumeric():
-                fromDomain = ''
-            else:
-                fromDomain = ".".join(fromADD.split('.')[-2:])
-            if toADD.replace('.','').isnumeric():
-                toDomain = ''
-            else:
-                toDomain = ".".join(toADD.split('.')[-2:])
-            dbInsert(csvwriter, {
-                'time':timeStamp,
-                'fromMAC': fromMAC.strip(),
-                'toMAC':toMAC.strip(),
-                'type':typ.strip(),
-                'length':int(length.replace('length','').strip()),
-                'fromADD':fromADD.strip(),
-                'toADD':toADD.strip(),
-                'toPort':toPort,
-                'fromPort':fromPort,
-                'fromDomain':fromDomain,
-                'toDomain':toDomain
-                })
-        except:
+            nextParts = mainParts[2].split(':')
+            path = nextParts[1]
+            length = int(nextParts[0][7:])
+            if path not in db: db[path] = 0
+            db[path]+= length
+
+            # timeStamp = mainParts[0].split(' ')[0]
+            # fromMAC, toMAC = mainParts[0].replace(timeStamp, "").replace(" (oui Unknown)", "").split('>')
+            # typ = mainParts[1]
+            # fromADD, toADD = mainParts[2].split(':')[1].split('>')
+            # fromPort = fromADD.strip().split('.')[-1]
+            # toPort = toADD.strip().split('.')[-1]
+            # fromADD = fromADD.replace('.'+fromPort+' ','').strip()
+            # toADD = toADD.replace('.'+toPort,'').strip()
+            # if fromADD.replace('.','').isnumeric():
+            #     fromDomain = ''
+            # else:
+            #     fromDomain = ".".join(fromADD.split('.')[-2:])
+            # if toADD.replace('.','').isnumeric():
+            #     toDomain = ''
+            # else:
+            #     toDomain = ".".join(toADD.split('.')[-2:])
+            # dbInsert(csvwriter, {
+            #     'time':timeStamp,
+            #     'fromMAC': fromMAC.strip(),
+            #     'toMAC':toMAC.strip(),
+            #     'type':typ.strip(),
+            #     'length':int(length.replace('length','').strip()),
+            #     'fromADD':fromADD.strip(),
+            #     'toADD':toADD.strip(),
+            #     'toPort':toPort,
+            #     'fromPort':fromPort,
+            #     'fromDomain':fromDomain,
+            #     'toDomain':toDomain
+            #     })
+            # db.setdefault(from,{}).setdefault(row['fromDomain'],[]).append(row['length'])
+
+            if lastmin != time.localtime()[4]:
+                dbInsert(None, db)
+                db = {}
+                lastmin = time.localtime()[4]
+
+        except ValueError:
             log("Bad row: {}".format(row.strip()))
 
-    f.close()
-    csvf.close()
+    for i in db:
+        print("{}: {}".format(i, db[i]))
