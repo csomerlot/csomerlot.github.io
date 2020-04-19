5c79b48644060b2199222843ee9f6b99f703d107
Chris Soerlot <csomerlot@gmail.com>
2020-04-19 08:03:40 -0400

    code cleanup

---
+++ code/tjsTurtle.py
@@ -0,0 +1,30 @@
+from turtle import *
+
+
+color('black', 'green')
+begin_fill()
+while True:
+    forward(200)
+    left(200)
+    if abs(pos()) < 1:
+        break
+end_fill()
+right(100)
+circle(100)
+done()
+        break
+penup()
+left()
+pendown()
+break
+color('black', 'green')
+begin_fill
+while True:
+    foward(200)
+    left(200)
+    if abs(pos()) < 2:
+break
+end_fill
+right(100)
+circle(100)
+done()
