9c2cadc796885a43a537c9304183e5daaafce7af
Chris Somerlot <csomerlot@brwncald.com>
2023-12-31 15:28:16 -0500

    Cleanup before the conversion

--- s3.bat
+++
@@ -1,2 +0,0 @@
-call C:\ProgramData\Anaconda3\Scripts\activate.bat
-aws s3 sync ./ s3://projects.somerlot.net --exclude "*" --include "*.html" --include "*.png" --include "*.jpg" --include "*.svg" --include "*.pdf" --acl public-read --delete
