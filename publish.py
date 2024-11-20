23d864807fa2f11026621ce02746355c61b08941
Chris <csomerlot@gmail.com>
2024-11-20 08:51:10 -0500

    Refactoring

--- publish.py
+++
@@ -1,8 +0,0 @@
-import os, glob
-
-for f in glob.glob("*.html")+glob.glob(".*.html"):
-    with open(f) as of: contents = of.read()
-    with open(os.path.join('docs',f.replace("._","")), 'w') as of:
-        of.write(contents.replace("._","").replace('img src="', 'img src="https://raw.githubusercontent.com/csomerlot/csomerlot.github.io/master/'))
-    os.remove(f)
-    print("Moved {}".format(f))
